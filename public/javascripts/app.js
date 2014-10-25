bf157249f59cc884d6dec1c1536d579abd2cd731
Sherwin Yu <sherwin.x.yu@gmail.com>
2014-10-24 17:20:08 -0700

    Add downKeys array and check in keyDown handler to check whether the key is already down before playing the sound

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -4,32 +4,39 @@ $(document).ready(function(){
   var counterUp = 0;
   var counterDown = 0;
 
-  for(var i=0;i<11;i++){
+  for(var i = 0; i < 11; i++) {
     audioup[i] = new Audio('keyup.mp3');
   }
 
-  for(var i=0;i<11;i++){
+  for(var i = 0; i < 11; i++) {
     audiodown[i] = new Audio('keydown.mp3');
   }
 
 
   var incUp = function(){
-    if(counterUp < 10){ counterUp++;  } 
-    else            { counterUp = 0 }
-  }
+    counterUp ++;
+    counterUp %= 10;
+  };
   var incDown = function(){
-    if(counterDown < 10){ counterDown++;  } 
-    else            { counterDown = 0 }
-  }
+    counterDown ++;
+    counterDown %= 10;
+  };
 
-  $('#typing').keyup(function(){
+  downKeys = [];
+
+  $('#typing').keyup(function(e){
     console.log(counterUp);
     audioup[counterUp].play();
     incUp();
+    downKeys[e.which] = false;
   });
-  $('#typing').keydown(function(){
+
+  $('#typing').keydown(function(e){
+    if (downKeys[e.which])
+      return;
     console.log(counterDown);
     audiodown[counterDown].play();
     incDown();
+    downKeys[e.which] = true;
   });
 });
