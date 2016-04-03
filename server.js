cae0dd0c6d0e7e0c6285a4c97e0e2e3d5aecda73
Rewon Child <rewonc@gmail.com>
2016-04-02 22:26:19 -0700

    Change port to 8081

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 var debug = require('debug')('typing');
 var app = require('./app');
 
-app.set('port', process.env.PORT || 3001);
+app.set('port', process.env.PORT || 8081);
 
 var server = app.listen(app.get('port'), function() {
   debug('Express server listening on port ' + server.address().port);
