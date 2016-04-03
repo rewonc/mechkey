88b6d93e5bf4effbf769b25c9261863bda8bb85c
Rewon Child <rewonc@gmail.com>
2016-04-02 22:40:31 -0700

    Update to other server

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
-#!/usr/bin/env node
-var debug = require('debug')('typing');
-var app = require('./app');
+var http = require("http");
 
-app.set('port', process.env.PORT || 8081);
+http.createServer(function(request, response) {
+  response.writeHead(200, {"Content-Type": "text/plain"});
+  response.write("Hello World");
+  response.end();
+}).listen(process.env.PORT || 8888);
 
-var server = app.listen(app.get('port'), function() {
-  debug('Express server listening on port ' + server.address().port);
-});
+// #!/usr/bin/env node
+// var debug = require('debug')('typing');
+// var app = require('./app');
+
+// app.set('port', process.env.PORT || 8081);
+
+// var server = app.listen(app.get('port'), function() {
+//   debug('Express server listening on port ' + server.address().port);
+// });
